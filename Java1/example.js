a7b3e69f6a3a6c0b056e6f24d2ce122fd97bbfec
odeoodi <ode.ojala@metropolia.fi>
2024-11-05 10:37:36 +0200

    testi

---
+++ Java1/example.js
@@ -0,0 +1,9 @@
+
+
+console.log("Helou mualima, example puhuu")
+
+const nimi = prompt("Anna nimi")
+
+console.log(nimi)
+
+document.querySelector("#nimi").innerHTML = 'Hyvvee huomenta ' + nimi +'!'
