b0d8e9bad6d71f6b47a12f57560bd2c08a9b1d65
odeoodi <ode.ojala@metropolia.fi>
2024-11-07 10:22:14 +0200

    valmis

--- Moduuli 2/teht2-4.js
+++ Moduuli 2/teht2-4.js
@@ -1,13 +1,13 @@
 /* Write a program that asks the user for numbers until he gives zero.
 The given numbers are printed in the console from the largest to the smallest. (2p)*/
-'use strict'
-const numbers = []
+'use strict';
+const numbers = [];
 
-let number
+let number;
 do {
-  number = prompt("Give me a number, stop by giving a zero.")
-  numbers.push(number)
-} while (number !== "0")
+  number = prompt('Give me a number, stop by giving a zero.');
+  numbers.push(number);
+} while (number !== '0');
 
-const bigNumbers = numbers.sort((a,b) => b-a)
-console.log(bigNumbers)
+const bigNumbers = numbers.sort((a, b) => b - a);
+console.log(bigNumbers);
