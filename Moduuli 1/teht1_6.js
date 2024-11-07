2cb8b6a88137568f98523ee83bd6224c5b9179fc
odeoodi <ode.ojala@metropolia.fi>
2024-11-07 10:15:12 +0200

    valmis

--- Moduuli 1/teht1_6.js
+++ Moduuli 1/teht1_6.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const answer = confirm('Should I calculate the squareroot?');
 
 if (answer === true) {
---
+++ Moduuli 2/teht2-4.js
@@ -0,0 +1,13 @@
+/* Write a program that asks the user for numbers until he gives zero.
+The given numbers are printed in the console from the largest to the smallest. (2p)*/
+'use strict'
+const numbers = []
+
+let number
+do {
+  number = prompt("Give me a number, stop by giving a zero.")
+  numbers.push(number)
+} while (number !== "0")
+
+const bigNumbers = numbers.sort((a,b) => b-a)
+console.log(bigNumbers)
