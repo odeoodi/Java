cf817600217539403141dce91ee9879f878be2a3
odeoodi <ode.ojala@metropolia.fi>
2024-11-19 11:49:37 +0200

    valmis

--- Moduuli 4/teht4-2.js
+++ Moduuli 4/teht4-2.js
@@ -1,16 +1,22 @@
-'use strict'
+'use strict';
 
-const moviesForm = document.querySelector('#movie-form')
+const moviesForm = document.querySelector('#movie-form');
 
 moviesForm.addEventListener('submit', async function(evt) {
-  evt.preventDefault()
-  const movie = document.querySelector('input[name=q]').value
+  evt.preventDefault();
+  const movie = document.querySelector('input[name=q]').value;
   try {
-    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${movie}`, {mode: 'no-cors'})
-    const jsonData = await response.json()
-    console.log(jsonData)
+    const response = await fetch(
+        `https://api.tvmaze.com/search/shows?q=${movie}`, {mode: 'cors'});
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const jsonData = await response.json();
+    jsonData.forEach(item => {
+      console.log('show ', item.show.name);
+    });
+    console.log(jsonData.show);
+  } catch (error) {
+      console.log('error ', error.message);
   }
-  catch (error) {
-    console.log(error.message)
-  }
-})
+});
